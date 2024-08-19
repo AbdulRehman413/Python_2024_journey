1d38b5ecb2c1fc925838734894ef68309667ce20
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-08-19 13:23:37 -0700

    Nested if statements

---
+++ just.py
@@ -0,0 +1,11 @@
+num = 18
+num = int(input("enter some shit"))
+if num<0:
+    print("number is negative")
+elif num>0:
+    if num<=10:
+        print("number is bewteen 1-10")
+    elif (num>10 and num<=20):
+        print("nummber is  bewteen 11-20")
+    else:
+        print("number is greater than 20")
