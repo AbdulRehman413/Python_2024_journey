052e012c7a6659f490426cd51ec9a509b0967613
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-08-21 00:20:35 -0700

    match cases

--- demo.py
+++ demo.py
@@ -1,19 +1,10 @@
-time = int(input("Please enter the current time:"))
-region= str(input("enter the region (AM/PM only):")).upper().strip()
-
-if region == "AM":
-    if time in range (1,12):
-        print("Good morning sir")
-    elif time == 12:
-        print("Good noon sir ")
-elif region == "PM":
-    if time in range (12,6):
-        print*("Good afternoon sir")
-    elif time == 6:
-        print("Good evening sir ")
-    elif time in range (7,12):
-        print("Good night sir ")
-    elif time == 12 :
-        print ("good night sir")
-else:
-    print("enter the correct region and time")
+x = int(input("enter the value of x:"))
+match x:
+    case 1:
+        print("x is equal to 544833")
+    case 2:
+        print("x is 123333")
+    case _ if x!=90:
+        print("case is not equal to 90")
+    case _ if x!=80:
+        print("case is not equal to 80")
--- new.py
+++ new.py
@@ -1,19 +1,8 @@
-time = int(input("Please enter the current time:"))
-region= str(input("enter the region (AM/PM only):")).upper().strip()
+x = int(input("enter the value of x: "))
+match x:
+    case 0:
+        print("x is zero")
 
-if region == "AM":
-    if time in range (1,12):
-        print("Good morning sir")
-    elif time == 12:
-        print("Good noon sir ")
-elif region == "PM":
-    if time in range (12,6):
-        print*("Good afternoon sir")
-    elif time == 6:
-        print("Good evening sir ")
-    elif time in range (7,11):
-        print("Good night sir ")
-    elif time == 12 :
-        print ("good night sir")
-else:
-    print("enter the correct region and time")
+        case _ if x!=90:
+        print("x is not 90")
+        i
