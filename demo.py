1c5ca34b61724247ab6066ca0a1714845c973e14
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-08-21 23:05:29 -0700

    for loop and range comand

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-x = int(input("enter the value of x:"))
-match x:
-    case 1:
-        print("x is equal to 544833")
-    case 2:
-        print("x is 123333")
-    case _ if x!=90:
-        print("case is not equal to 90")
-    case _ if x!=80:
-        print("case is not equal to 80")
+colors= ["red", "white", "green", "yellow"]
+for color in colors:
+    print(color)
+    for char in color:
+        print(char)
+
+
+
+for k in range(1,12,2):
+    print(k)
