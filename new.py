54bdb1cd9486399fbb46a4a28a272412e29f4435
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-08-22 12:10:25 -0700

    functions(def)

---
+++ new.py
@@ -0,0 +1,30 @@
+def calculategmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isgreater(a,b):
+    if (a>b):
+        print("first number is greater")
+    else:
+        print("second number is greater")
+def islesser(a,b):
+    if (a<b):
+        print("first number is lesser")
+    else:
+        print("second number is lesser")
+def isequal(a,b):
+
+    pass
+
+
+a = 7
+b = 8
+isgreater(a,b)
+islesser(a,b)
+calculategmean(a,b)
+
+c = 8
+d = 9
+isgreater(c,d)
+islesser(c,d)
+calculategmean(c,d)
