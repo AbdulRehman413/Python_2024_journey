b231581b8860b0b6c7d9dffd45d7bdd57826b5d0
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-08-27 11:23:20 -0700

    docstrings

--- new.py
+++ new.py
@@ -4,17 +4,3 @@ def square(n):
 square(7)
 print(square.__doc__)
 
-letter = "my name is {} and i am from {}"
-country= "pakistan"
-name = "Abdul Rehman"
-
-print(letter.format(name, country))
-print(f"my name is {name} and i live in {country}")
-
-
-price = 49.9999
-seller = f"for only {price:.2f} dollars"
-print(seller)
-
-print(f"{2*49}")
-print(type(seller))
