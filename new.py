3bd17545c78d04960756036c3c9e9dcf9bf0fe65
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-08-28 05:23:43 -0700

    recursions

--- new.py
+++ new.py
@@ -1,6 +1,11 @@
-def square(n):
-    '''take the value and returns its square'''
-    print(n**2)
-square(7)
-print(square.__doc__)
+def factorial(n):
+    if n ==0 or n ==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
 
+print(factorial(3))
+print(factorial(7))
+print(factorial(5))
+print(factorial(0))
