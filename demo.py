1bcef5e9229d33e439feea493fed1caf24fc11bc
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-09-15 23:48:58 +0500

    short hand if else

---
+++ demo.py
@@ -0,0 +1,5 @@
+age = int(input("enter your age: "))
+if age >= 18:
+    print("welcome to our website")
+else:
+    print("your not old enough")
--- new.py
+++ new.py
@@ -1,28 +1,3 @@
-choice = int(input("1 for code and 2 for decode: "))
-if choice == 1:
-    code  = (input("enter the value:"))
-    coding = True
-    if coding:
-        if (len(code)>=3):
-            a1 = "jjd"
-            a2 = "sas"
-            encoded = a1 + code[1:] + code[0] + a2
-            print(encoded)
-    else:
-            print("your code should be at least 3 letters long")
-
-elif choice == 2:
-     code = input("enter the value")
-     coding = False
-    
-     if (len(code)>=3):
-        
-            code =  code[3:-3] 
-            decoded = code[-1] + code[:-1] 
-            print(decoded)
-     else:
-            print("enter correct value to decode")
-
-
-    
-    
+a = 999
+b = 88828
+print(a)if a>b else print("=")if a==b else print(b)
