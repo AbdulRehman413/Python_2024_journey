91915ec339737ebfddebd0cabb59fa44063435fa
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-10-26 18:21:26 +0500

    practice

--- demo.py
+++ demo.py
@@ -1,5 +1,25 @@
-age = int(input("enter your age: "))
-if age >= 18:
-    print("welcome to our website")
-else:
-    print("your not old enough")
+choice = int(input("Press 1 for Code and 2 for Decode: "))
+if choice == 1:
+    code  = (input("Enter the value you want to code: "))
+    coding = []
+    coding = True
+    if coding:
+        if (len(code)>=3):
+            a1 = "jsd"
+            a2 = "san"
+            encoded = a1 + code[1:] + code[0] + a2
+            print(encoded)
+    else:
+            print("Your Code should be at least 3 letters long.")
+
+elif choice == 2:
+     code = input("Enter the value you want to decode: ")
+     coding = False
+    
+     if (len(code)>=3):
+        
+            code =  code[3:-3] 
+            decoded = code[-1] + code[:-1] 
+            print(decoded)
+     else:
+            print("enter correct value to decode")
--- new.py
+++ new.py
@@ -1,3 +1,22 @@
-a = 999
-b = 88828
-print(a)if a>b else print("=")if a==b else print(b)
+# marks = [12,66,45,98,49]
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if (index == 3):
+#         print("this results is of ibrahim's kashif")
+#     index +=1
+
+# for i in range(1,9):
+#     print("*"*i)
+
+
+
+a = input("enter the value: ")
+
+for i in range(1,11):
+    print(f"{int(a)} X {i} = {int(a)*i}")
+
+# a = int(input("enter the value for table:"))
+
+# for i in range(1,12):
+#     print(f"{int(a)} X {i} = {int(a)*i}")
