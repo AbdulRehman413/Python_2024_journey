de19394b7d86db0bf0f183f244b269281fa76c3d
Abdul Rehman <abdul.rehman51048@gmail.com>
2024-11-01 22:53:38 +0500

    proper code to find leap year

--- new.py
+++ new.py
@@ -1,8 +1,6 @@
-year = int(input("enter total days of feburary: "))
-if year == 29:
+year = int(input("Enter year to find if its leap or not:  "))
+if year % 4 ==0:
     print("this year is a leap year")
-elif year > 29:
-    print("feburary doesnt have that many days, input number of days should be 29 or under 29")
 else:
     print("this year is not a leap year")
-print("An indication of leap year is that the month feburary will have 29 days")
+
