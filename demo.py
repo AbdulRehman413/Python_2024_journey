0fff14e8afa65261806fddfec63900b75ec1ae8d
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-01-26 23:09:14 +0500

    __name__ function

--- demo.py
+++ demo.py
@@ -1,25 +1,4 @@
-choice = int(input("Press 1 for Code and 2 for Decode: "))
-if choice == 1:
-    code  = (input("Enter the value you want to code: "))
-    coding = []
-    coding = True
-    if coding:
-        if (len(code)>=3):
-            a1 = "jsd"
-            a2 = "san"
-            encoded = a1 + code[1:] + code[0] + a2
-            print(encoded)
-    else:
-            print("Your Code should be at least 3 letters long.")
-
-elif choice == 2:
-     code = input("Enter the value you want to decode: ")
-     coding = False
-    
-     if (len(code)>=3):
-        
-            code =  code[3:-3] 
-            decoded = code[-1] + code[:-1] 
-            print(decoded)
-     else:
-            print("enter correct value to decode")
+def welcome():
+    print("welcome")
+    if __name__ == "__demo__":
+        welcome()
--- new.py
+++ new.py
@@ -1,6 +1,2 @@
-from math import sqrt , pi
-result = sqrt(16)* pi
-print(result)
-
-import math
-print(dir(math))
+import demo
+demo.welcome()
