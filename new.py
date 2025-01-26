200422bd92e3f884c89af68be13a9d5e3125e947
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-01-26 17:06:52 +0500

    enumerate function

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
-a = 333
-b = 2929
-print("a") if a>b else print("b") if a<b else print("B")
+marks = [12,34,45,65,34,99]
+for index, mark in enumerate(marks, start=1):
+    print(mark)
+    if (index==5):
+        print("you've got shit marks")
