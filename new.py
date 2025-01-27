be2170c8ef1e314e5a5c0bd54b6b430913820333
Abdul Rehman <abdul.rehman51048@gmail.com>
2025-01-27 23:51:13 +0500

    code and decode

--- new.py
+++ new.py
@@ -1,12 +1,25 @@
-import os
+choice = int(input("Press 1 for Code and 2 for Decode: "))
+if choice == 1:
+    code  = (input("Enter the value you want to code: "))
+    coding = []
+    coding = True
+    if coding:
+        if (len(code)>=3):
+            a1 = "jsd"
+            a2 = "san"
+            encoded = a1 + code[1:] + code[0] + a2
+            print(encoded)
+    else:
+            print("Your Code should be at least 3 letters long.")
 
-for i in range(0,100):
-    os.rename(f"data/day{i+1}" , (f"data/nigga{i+1}"))
-import os
-folder = os.listdir("data")
-print(folder)
-
-
-for folders in folder:
-    print(folders)
-    print (os.listdir(f"data/{folders}"))
+elif choice == 2:
+     code = input("Enter the value you want to decode: ")
+     coding = False
+    
+     if (len(code)>=3):
+        
+            code =  code[3:-3] 
+            decoded = code[-1] + code[:-1] 
+            print(decoded)
+     else:
+            print("enter correct value to decode")
--- practice.py
+++ practice.py
@@ -641,3 +641,17 @@
 #             print(decoded)
 #      else:
 #             print("enter correct value to decode")
+
+
+# import os
+
+# for i in range(0,100):
+#     os.rename(f"data/day{i+1}" , (f"data/nigga{i+1}"))
+# import os
+# folder = os.listdir("data")
+# print(folder)
+
+
+# for folders in folder:
+#     print(folders)
+#     print (os.listdir(f"data/{folders}"))
