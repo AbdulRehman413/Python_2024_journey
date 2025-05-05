27fb2a2823d7e9662e08876330c6960a5a0028a1
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-05 23:19:22 +0500

    constructor

--- demo.py
+++ demo.py
@@ -1,13 +1,43 @@
+# class person:
+    
+#     name = "abdul"
+#     occupation = "student"
+#     networth = 0 
+
+#     def info(self):
+#         print(f"{self.name} is a {self.occupation} with {self.networth} networth")
+    
+# a = person()
+# b = person()
+# c = person()
+# a.name = "hassan"
+# a.occupation = "developer"
+
+# b.name = "umar"
+# b.occupation = "unemployed"
+# a.info()
+# b.info()
+# c.info()
+
+
+
 class person:
     
-    name = "abdul"
-    occupation = "student"
-    networth = 0 
+    
+
+    def __init__(self,n,o):
+        print("i am a person")
+        self.name = n
+        self. occ = o
 
     def info(self):
-        print(f"{self.name} is a {self.occupation} with {self.networth} networth")
+           print(f"{self.name} is a {self.occ}")
     
-a = person()
-a.name = "hassan"
-a.occupation = "developer"
+a = person("abdul", "student")
+b = person("hassan", "developer")
+# c = person()
+
 a.info()
+b.info()
+# c.info()
+
