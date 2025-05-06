0c8abaf6b6a6690e8e727dfee7332c83c48b8814
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-06 14:14:12 +0500

    getters and setters

--- demo.py
+++ demo.py
@@ -43,15 +43,34 @@
 
 
 
-def greet(fx):
-    def mfx():
-        print("Good morning")
-        fx()
-        print("Thanks for using this function")
-    return mfx
+# def greet(fx):
+#     def mfx():
+#         print("Good morning")
+#         fx()
+#         print("Thanks for using this function")
+#     return mfx
     
-@greet 
-def hello():
-    print("hello world ")
+# @greet 
+# def hello():
+#     print("hello world ")
 
-hello()
+# hello()
+
+
+class myclass:
+    def __init__(self, value):
+        self._value = value
+    def show(self):
+        print(f"value is {self._value}")
+    @property
+    def ten_value(self):
+        return 10*self._value
+    
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self._value = new_value/10
+       
+obj = myclass(10)
+obj.ten_value = 67
+obj.show()
+print(obj.ten_value)
