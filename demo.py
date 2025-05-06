fa5e9f6c4521329c19f63241d88773db48e55b4a
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-06 14:32:48 +0500

    inheritance

--- demo.py
+++ demo.py
@@ -57,20 +57,44 @@
 # hello()
 
 
-class myclass:
-    def __init__(self, value):
-        self._value = value
-    def show(self):
-        print(f"value is {self._value}")
-    @property
-    def ten_value(self):
-        return 10*self._value
+# class myclass:
+#     def __init__(self, value):    #class value was 10 , updated the value to 67 and in getter it got multiplied by 10 became 6700
+#         self._value = value       # then in setter it got divided by 10 
+#     def show(self):
+#         print(f"value is {self._value}")
+#     @property   #getter 
+#     def ten_value(self):
+#         return 10*self._value
     
-    @ten_value.setter
-    def ten_value(self, new_value):
-        self._value = new_value/10
+#     @ten_value.setter  #setter 
+#     def ten_value(self, new_value):
+#         self._value = new_value/10
        
-obj = myclass(10)
-obj.ten_value = 67
-obj.show()
-print(obj.ten_value)
+# obj = myclass(10)
+# obj.ten_value = 67
+# obj.show()
+# print(obj.ten_value)
+
+
+
+
+class employee:
+    def __init__(self , name, id):
+        self.name = name
+        self.id = id
+
+
+    def showdetails(self):
+        print(f"the name of employee is {self.name} and his id is {self.id}")
+
+
+class programmer(employee):
+    def showlan(self):
+        print("The default language is python")
+
+a = employee("rohan Das", 6969)
+a.showdetails()
+a1 = programmer ("harry", 7777)
+a1.showdetails()
+a1.showlan()
+
