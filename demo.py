3f0325f37c062b8408cc138a3b218cc7204d72f0
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-06 14:50:30 +0500

    access modifiers

--- demo.py
+++ demo.py
@@ -78,23 +78,30 @@
 
 
 
+# class employee:
+#     def __init__(self , name, id):
+#         self.name = name
+#         self.id = id
+
+
+#     def showdetails(self):
+#         print(f"the name of employee is {self.name} and his id is {self.id}")
+
+
+# class programmer(employee):
+#     def showlan(self):
+#         print("The default language is python")
+
+# a = employee("rohan Das", 6969)
+# a.showdetails()
+# a1 = programmer ("harry", 7777)
+# a1.showdetails()
+# a1.showlan()
+
 class employee:
-    def __init__(self , name, id):
-        self.name = name
-        self.id = id
+    def __init__(self):
+        self.__name = "Harry"
 
-
-    def showdetails(self):
-        print(f"the name of employee is {self.name} and his id is {self.id}")
-
-
-class programmer(employee):
-    def showlan(self):
-        print("The default language is python")
-
-a = employee("rohan Das", 6969)
-a.showdetails()
-a1 = programmer ("harry", 7777)
-a1.showdetails()
-a1.showlan()
-
+a = employee()
+print(a._employee__name)
+print(a.__dir__())    
