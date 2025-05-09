c8fdee376225113b140100dea43b5da338e70f2d
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-09 21:03:23 +0500

    class methods

--- demo.py
+++ demo.py
@@ -155,22 +155,37 @@
 # after the program is stopped 
 
 
-class library:
-    noofbooks = 0
-    def __init__(self, name):       
-        self.name = name
-        library.noofbooks +=1
+# class library:
+#     noofbooks = 0
+#     def __init__(self, name):       
+#         self.name = name
+#         library.noofbooks +=1
 
-    def showbooks(self):
-        print(f"the books named {self.name} are {self.noofbooks} in this library")
+#     def showbooks(self):
+#         print(f"the books named {self.name} are {self.noofbooks} in this library")
 
-a = library("atomic habits")
-# a.noofbooks = 7
-a.showbooks()
-b = library("The 5AM club")
-# b.noofbooks = 18
-b.showbooks()
-    
-
+# a = library("atomic habits")
+# # a.noofbooks = 7
+# a.showbooks()
+# b = library("The 5AM club")
+# # b.noofbooks = 18  
+# b.showbooks()
 
 
+
+class employee:
+    company = "apple"
+    def show(self):
+        print(f"The name of the company is {self.company} in which {self.name} works ")
+
+    @classmethod
+    def changecompanyname(cls, newcompany):
+        cls.company = newcompany
+
+
+a = employee()
+a.name = ("harry")
+a.show()
+a.changecompanyname("tesla")
+a.show()
+print(employee.company)
