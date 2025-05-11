06ce99fda12a7131c708d394b43633d4a4796c04
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 03:00:32 +0500

    file handling

--- import and file handling/file handling.py
+++ import and file handling/file handling.py
@@ -1,17 +1,17 @@
 
-print("wasup")
-import os
+print("wasupp")
+# import os
 
-for i in range(0,100):
-    os.rename(f"data/day{i+1}" , (f"data/nigga{i+1}"))
-import os
-folder = os.listdir("data")
-print(folder)
+# for i in range(0,100):
+#     os.rename(f"data/day{i+1}" , (f"data/nigga{i+1}"))
+# import os
+# folder = os.listdir("data")
+# print(folder)
 
 
-for folders in folder:
-    print(folders)
-    print (os.listdir(f"data/{folders}"))
+# for folders in folder:
+#     print(folders)
+#     print (os.listdir(f"data/{folders}"))
 
 
 #create a folder in local disc C and name it anything, then open that folder with vs code and write this code there 
