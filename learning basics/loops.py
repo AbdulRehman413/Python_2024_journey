1e3823b69aa3dbd7dab26278eeb946dc826f6535
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 01:57:01 +0500

    loops

---
+++ learning basics/loops.py
@@ -0,0 +1,16 @@
+a = ("abdul")
+print(a[2])
+print(a[0])
+print(a[3])
+
+for character in a:
+    print(character)
+
+
+a = ("pneumonoultramicrosilicovolcanoconiosis")
+for character in a:
+    print(character)
+
+
+a = ("pneumonoultramicroscopicsilicovolcanoconiosis")
+print("Lung disease is also named as " , a , " which has ", len(a), "characters")
