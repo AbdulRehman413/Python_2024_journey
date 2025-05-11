b5cf274fac4e76be35eff42f95cc4e39ed754814
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 01:36:37 +0500

    data types

---
+++ data types.py
@@ -0,0 +1,15 @@
+a = complex(2+33) #integers
+b = False     #boolean
+c = "Abdul Rehman"   #string
+
+print(a , b, c, sep="-" )
+
+list1 = [8 , 2.2, [-4, 5], ["apple, nigga"]]   #lists 
+print(list1)
+
+dict1 = {"name" : "abdul rehman" ,"age": 17,   #dictionary
+          "can vote" : None  }
+print(dict1)
+
+tuple1 = (("parrot , nigga , gigga "))  #tuple 
+print(tuple1)
