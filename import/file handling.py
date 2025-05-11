1a4f90d7bf008143e5e442dad8c15aa4e9566777
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 02:52:24 +0500

    file handling

---
+++ import/file handling.py
@@ -0,0 +1,16 @@
+import os
+
+for i in range(0,100):
+    os.rename(f"data/day{i+1}" , (f"data/nigga{i+1}"))
+import os
+folder = os.listdir("data")
+print(folder)
+
+
+for folders in folder:
+    print(folders)
+    print (os.listdir(f"data/{folders}"))
+
+
+#create a folder in local disc C and name it anything, then open that folder with vs code and write this code there 
+#this code will make 100 new folders in that folder
