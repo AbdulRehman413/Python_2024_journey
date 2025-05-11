5e44ee54b902f9640bc8e7543bf1e7b80f4977f8
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 02:19:50 +0500

    cheap KBC (cringe)

---
+++ projects/cheap KBC.py
@@ -0,0 +1,20 @@
+l = ["Question no.1: Which country has the most satellites in the world"]
+A = ["A)Russia"]
+B = ["B)China"]
+C = ["C)Japan"]
+D = ["D)America"]
+
+print (l)
+
+print(A)
+print(B)
+print(C)
+print(D)
+q = str(input("So what's your final answer:")).upper()
+
+if q == "A":
+    print("7 CROREEEEEEEE")
+else:
+    print("APKA JAWAABBBBBBBBBBBBB ....... GALAT HAI , AP KHALI HATH JAYEN GAY")
+
+    
