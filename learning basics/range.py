4dda848d4d158820ee00bcb0b3685a7b45133dbd
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 02:06:49 +0500

    range in loop

---
+++ learning basics/range.py
@@ -0,0 +1,14 @@
+colors= ["red", "white", "green", "yellow"]
+for color in colors:
+    print(color)
+    for char in color:
+        print(char)
+
+
+
+for k in range(1,12,2):
+    print(k)
+
+i = 0
+for i in range(1,100):
+    print(i)
