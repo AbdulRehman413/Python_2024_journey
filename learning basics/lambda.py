978e723b6633315fc62a36cdaada0b9bd14464d6
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 09:37:19 +0500

    lambda function

---
+++ learning basics/lambda.py
@@ -0,0 +1,11 @@
+# def double(x):
+#     return x*2
+double = lambda x: x*2
+cube = lambda x: x*x*x
+avg = lambda x,y: (x+y)/2 
+print(avg(60, 32))
+print(double(5))
+print(cube(69))
+
+
+# instead 0f using def function , its alot better to use lambda as its much easier to use
