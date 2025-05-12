a7306f7625936735e63d5cd2dea8d30167cdbf49
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 09:50:56 +0500

    decorators in oops

---
+++ OOPS python/decorator.py
@@ -0,0 +1,12 @@
+def greet(fx):
+    def mfx():
+        print("Good morning")
+        fx()
+        print("Thanks for using this function")
+    return mfx
+    
+@greet 
+def hello():
+    print("hello world ")
+
+hello()
