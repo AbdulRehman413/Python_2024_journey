f393ffa15744b80057029db0b39b2630757ee299
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 09:48:54 +0500

    class in oops

--- OOPS python/class.py
+++ OOPS python/class.py
@@ -8,6 +8,13 @@ class person:
         print(f"{self.name} is a {self.occupation} with {self.networth} networth")
     
 a = person()
+b = person()
+c = person()
 a.name = "hassan"
 a.occupation = "developer"
+
+b.name = "umar"
+b.occupation = "unemployed"
 a.info()
+b.info()
+c.info()
