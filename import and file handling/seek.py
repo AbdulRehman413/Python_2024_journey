a61557b247627e803c70a5eaeced5a81300796d1
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-05-12 09:34:48 +0500

    seek function

---
+++ import and file handling/seek.py
@@ -0,0 +1,10 @@
+# with open('myfile.txt', 'r') as f:
+#     print(type(f))
+#     f.seek(10)
+#     data = f.read(5)
+#     print(data)
+
+
+print("Set the stream position, and return the new stream position.")
+print("cookie Zero or an opaque number returned by tell().")
+print("whence The relative position to seek from Four operations are supported, given by the following argument combinations:")
