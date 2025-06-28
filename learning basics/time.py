43697edec762e035b8852e3e6185c3ac5f60ff5a
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-06-28 15:20:08 +0500

    time module

---
+++ learning basics/time.py
@@ -0,0 +1,21 @@
+import time
+def usingfor():
+    i = 0
+    for i in range(5000):
+        print(i)
+
+def usingwhile():
+    i = 0
+    while i<5000:
+        i = i +1
+        print(i)
+
+init = time.time()
+usingfor()
+ti = time.time() - init
+init = time.time()
+usingwhile()
+init = time.time()-init
+print(ti)
+
+
